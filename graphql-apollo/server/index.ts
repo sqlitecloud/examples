eaf164a5b30a21120eeacb62c717dfaf02d86bb1
unatarajan <38731992+unatarajan@users.noreply.github.com>
2024-07-23 17:05:18 -0400

    Added and tested resolvers for join queries and mutations (i.e. inserts); authored by Jacob Prall

--- graphql-apollo/server/index.ts
+++ graphql-apollo/server/index.ts
@@ -4,7 +4,7 @@ import { Database } from '@sqlitecloud/drivers';
 
 const connStr = Bun.env.CONN_STR || ':memory:';
 
-const db = new Database(connStr)
+const db = new Database(connStr);
 
 const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
@@ -41,12 +41,26 @@ const typeDefs = `#graphql
     Name: String
   }
 
+  type Join {
+    AlbumId: Int
+    Title: String
+    ArtistName: String
+  }
+
   type Query {
-    albums: [Album],
-    artists: [Artist],
-    tracks: [Track],
-    genres: [Genre],
+    albums: [Album]
+    artists: [Artist]
+    tracks: [Track]
+    genres: [Genre]
     mediaTypes: [MediaType]
+    joins: [Join]
+    artist(name: String): Artist
+    albumsByArtist(artistId: Int): [Album]
+  }
+
+  type Mutation {
+    createArtist(name: String): Artist
+    createAlbum(title: String, artistId: Int): Album
   }
 `;
 
@@ -67,13 +81,42 @@ const resolvers = {
     mediaTypes: async () => {
       return await db.sql`SELECT * FROM media_types`;
     },
+    artist: async (_: any, { name }: { name: string }) => {
+      const res = await db.sql`SELECT * FROM artists WHERE Name LIKE ${name};`;
+      if (res.length === 0) return null;
+      return res[0];
+    },
+    albumsByArtist: async (_: any, { artistId }: { artistId: number }) => {
+      return await db.sql`SELECT albums.AlbumId, albums.Title FROM albums INNER JOIN artists ON albums.ArtistId = artists.ArtistId WHERE artists.ArtistId = ${artistId}`;
+    },
+  },
+  Mutation: {
+    createArtist: async (_: any, args: { name: string }) => {
+      const res =
+        await db.sql`INSERT INTO artists (Name) VALUES (${args.name})`;
+      if (res.changes === 0) return null;
+      return { ArtistId: res.lastID, Name: args.name };
+    },
+    createAlbum: async (_: any, args: { title: string; artistId: number }) => {
+      const res =
+        await db.sql`INSERT INTO albums (Title, ArtistId) VALUES (${args.title}, ${args.artistId})`;
+      if (res.changes === 0) return null;
+      return {
+        AlbumId: res.lastID,
+        Title: args.title,
+        ArtistId: args.artistId,
+      };
+    },
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+  listen: { port: 4000 },
 });
-  
+
 console.log(`🚀  Server ready at: ${url}`);
